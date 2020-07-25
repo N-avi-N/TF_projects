c9ad79eae41a56e53f0f2266edaaeb8d5096ee3f
N-avi-N <navin_k212@yahoo.com>
2020-07-24 20:16:52 -0500

    First Commit !!

--- MNIST_digit_recognition.py
+++ MNIST_digit_recognition.py
@@ -10,13 +10,13 @@ class myCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train/255.0, x_test/255.0
+x_train, x_test = x_train / 255.0, x_test / 255.0
 callbacks = myCallback()
 
 model = tf.keras.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
                              tf.keras.layers.Dense(512, activation=tf.nn.relu),
                              tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+                             ])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
